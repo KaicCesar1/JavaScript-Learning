08a4094c706fc3d224eef383c0004ab3fa8edc71
KaicCesar1 <kaic_cesar@hotmail.com>
2024-10-09 14:32:28 -0300

    Update estruturas

--- estruturas/JS/estrutura-condicional.js
+++ estruturas/JS/estrutura-condicional.js
@@ -1,2 +1,34 @@
 // Estruturas de Controle
 
+
+const num = 18;
+
+if (num > 5) {
+    console.log("num é maior que 5");
+} 
+
+const user = "Joao";
+
+if (user === "Joao") {
+    console.log(`Ola ${user}`);
+}
+
+
+const loggedIn = true;
+
+if (loggedIn) {
+    console.log("Está autênticado");
+} else {
+    console.log("Não está autênticado");
+}
+
+
+if (1 > 2) {
+    console.log("Teste 1")
+} else if (2 > 3) {
+    console.log("Teste 2")
+} else if (3 > 4) {
+    console.log("Teste 3")
+} else {
+    console.log("Saiu")
+}
---
+++ estruturas/JS/estruturas-de-repeticao.js
@@ -0,0 +1,36 @@
+console.log("-------------");
+
+// While
+
+i = 5;
+
+while (i != 0) {
+    console.log(i);
+    i = i - 1;
+}
+
+// Do While
+let p = 5;
+
+do {
+    console.log(`Valor de o: ${p}`);
+    p--
+} while (p > 0)
+
+
+// FOR 
+
+// Variável fora do for
+let y;
+
+for(y = 5; y > 0; y--) {
+    console.log(y);
+}
+
+
+// Variável dentro do for
+for(let x = 0; x < 5; x++) {
+    console.log(x)
+}
+
+
