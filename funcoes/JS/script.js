a9f59663cddcbeb7403fd973b2da28bc08461c1f
KaicCesar1 <kaic_cesar@hotmail.com>
2024-10-16 14:18:19 -0300

    Update script.js

---
+++ funcoes/JS/script.js
@@ -0,0 +1,18 @@
+// 1 - Criando uma função
+
+function minhaFuncao() {
+    console.log("Testando")
+}
+
+minhaFuncao()
+
+
+function funcaoComParametro(txt) {
+    console.log(`imprimindo: ${txt}`)
+}
+
+funcaoComParametro("Kaic")
+funcaoComParametro("Gabriela")
+
+
+
