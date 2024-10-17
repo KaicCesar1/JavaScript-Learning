6e1547c6868753aef6d7f358ed0ef19a7ff1ba34
KaicCesar1 <kaic_cesar@hotmail.com>
2024-10-17 16:03:56 -0300

    update

--- Arrays/JS/script.js
+++ Arrays/JS/script.js
@@ -68,3 +68,128 @@ console.log(car);
 delete car.km;
 
 console.log(car);
+
+
+// 7 - Assign
+
+const obj = {
+    a: "teste",
+    b: true
+};
+
+console.log(obj instanceof Object)
+
+const obj2 = {
+    c: []
+};
+
+console.log(obj2);
+Object.assign(obj2, obj);
+console.log(obj2);
+
+
+// 8 - Método Key e Entries
+
+// Mostra somentes as chaves 
+console.log(Object.keys(obj))
+console.log(Object.keys(obj2))
+console.log(Object.keys(car))
+
+
+// Aqui mostra as chaves e valores
+console.log(Object.entries(obj))
+
+
+// 9 - Mutação
+
+const a = {
+    name: "Kaic"
+}
+
+const b = a;
+
+console.log(a);
+console.log(b);
+
+a.age = 31;
+
+console.log(a);
+console.log(b);
+
+delete b.age;   
+console.log(a);
+console.log(b);
+
+
+// 10 - Loop e Arrays
+const users = ["Kaic", "Mark", "Elon", "Bill", "Matheus"];
+
+for(let i = 0; i < users.length; i++){
+    console.log(`Listando o usuário: ${users[i]}`);
+}
+
+
+// 11 - Push e Pop
+
+const array = ["a", "b", "c"];
+
+
+array.push("d");
+
+console.log(array);
+
+array.pop();
+
+console.log(array);
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido);
+
+array.push("m", "n", "o");
+
+console.log(array);
+
+
+// 12 - Shift e Unshift
+
+const letters = ["a", "b", "c"];
+
+const letter = letters.shift()
+
+console.log(letters);
+console.log(letter);
+
+letters.unshift("a");
+console.log(letters);
+
+// exemplo Pop/Push e Shift e Unshift
+
+const alphabet = ["a", "b", "c", "d"];
+alphabet.pop(); // remove o último elemento
+alphabet.shift(); // remove o primeiro elemento
+
+console.log(alphabet);
+
+alphabet.unshift("a"); // adciona os elementos no inicio do array
+alphabet.push("d"); // adiciona os elementos no fim do array
+
+console.log(alphabet);
+
+// 13 - indexOf e lastIndexOf
+
+const myElements = ["morango", "maça", "pera", "abacate", "pera"];
+
+console.log(myElements.indexOf("maça"));
+console.log(myElements.indexOf("pera"));
+
+console.log(myElements.lastIndexOf("morango")); 
+
+
+// 14 - Slice
+
+const testSlice= ["a", "b", "c", "d", "e"];
+
+const subArray = testSlice.slice(2, 4);
+console.log(testSlice);
+console.log(subArray);
